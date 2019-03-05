e93db7a91d70afa130595b9238f0fcc0e23204bd
Julia Gnann <julia.gnann@exxcellent.de>
2019-03-05 16:32:24 +0100

    Rest API für device types update19

--- falchsite/views.py
+++ falchsite/views.py
@@ -3,13 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import APIView
 from .models import *
 from .serializers import *
 
 # Create your views here.
 
-class Device_TypesViewSet(viewsets.ModelViewSet):
+class Device_TypesViewSet(APIView.ModelViewSet):
     """
     Provides a get method handler
     """
