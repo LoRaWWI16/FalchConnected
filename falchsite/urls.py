8e2bb04093b550f794c55f5168d62c16aadacdb5
Julia Gnann <julia.gnann@exxcellent.de>
2019-03-05 16:38:42 +0100

    Rest API für device types update22

--- falchsite/urls.py
+++ falchsite/urls.py
@@ -2,9 +2,5 @@ from django.conf.urls import url, include
 from .views import Device_TypesViewSet
 
 urlpatterns = [
-    url(r'device_types/', Device_TypesViewSet.as_view(
-        {
-            'get': 'list'
-        }
-    ), name="device_types-all")
+    url(r'device_types/', Device_TypesViewSet.as_view(), name="device_types-all")
 ]
