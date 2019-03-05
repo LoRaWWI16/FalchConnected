1b6138b569e502d0ab014c5b0688d05b0feea9d7
Meikaefer <berger-meike@web.de>
2019-03-05 18:22:18 +0100

    Output all Devices

--- falchsite/urls.py
+++ falchsite/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from .views import Device_TypesViewSet
 
 urlpatterns = [
-    url(r'device_types/', Device_TypesViewSet.as_view(), name="device_types-all")
+#    url(r'device_types/', Device_TypesViewSet.as_view(), name="device_types-all")
+    url(r'devices/', DevicesViewSet.as_view(), name="devices-all")
 ]
