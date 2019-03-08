945035c35f9c54a0942f5bac96c9c83ffdcd58e2
Meikaefer <berger-meike@web.de>
2019-03-08 08:30:29 +0100

    Output only one device fix

--- falchsite/urls.py
+++ falchsite/urls.py
@@ -4,6 +4,6 @@ from .views import DeviceViewSet
 
 urlpatterns = [
     url(r'device_types/', Device_TypesViewSet.as_view(), name="device_types-all"),
-    url(r'devices/', DeviceViewSet.as_view(), name="devices-all")
+    url(r'devices/', DeviceViewSet.as_view(), name="devices-all"),
     url(r'devices/', DeviceViewSet.as_view(), name="devices-one")
 ]
