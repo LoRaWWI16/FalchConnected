2c9e02ff4bd94d0205a4cb332792baa7f643461b
Meikaefer <berger-meike@web.de>
2019-03-09 12:51:32 +0100

    Error URLS Fix

--- falchsite/urls.py
+++ falchsite/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'device_types/', Device_TypesViewSet.as_view(), name="device_types-all"),
     url(r'devices/', DeviceViewSet.as_view(), name="devices-all"),
     url(r'^device/$', OneDeviceViewSet.as_view(), name="device_one"),
-    url(r'errors/, ErrorViewSet.as_view(), name="error-all")
+    url(r'errors/', ErrorViewSet.as_view(), name="error-all")
 ]
