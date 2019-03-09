7c76d353169e0bae38789217fe3bf0bf7631bc83
Meikaefer <berger-meike@web.de>
2019-03-09 12:49:55 +0100

    URL Erro Fix

--- falchsite/urls.py
+++ falchsite/urls.py
@@ -4,6 +4,6 @@ from .views import Device_TypesViewSet, DeviceViewSet, OneDeviceViewSet, ErrorVi
 urlpatterns = [
     url(r'device_types/', Device_TypesViewSet.as_view(), name="device_types-all"),
     url(r'devices/', DeviceViewSet.as_view(), name="devices-all"),
-    url(r'^device/$', OneDeviceViewSet.as_view(), name="device_one")
+    url(r'^device/$', OneDeviceViewSet.as_view(), name="device_one"),
     url(r'errors/, ErrorViewSet.as_view(), name="error-all")
 ]
