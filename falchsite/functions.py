3dd01b9bfa68249656955f707b1fd0e38466c815
Julia Gnann <julia.gnann@exxcellent.de>
2019-03-09 18:14:01 +0100

    Rest API list errors limit results

--- falchsite/functions.py
+++ falchsite/functions.py
@@ -3,11 +3,12 @@ from django.utils import timezone
 from django.shortcuts import render
 
 def error_list(request):
-    logs = Log.objects.all().order_by('timestamp').values('data', 'timestamp', 'signal', 'notification', 'device')
+    logs = Log.objects.all().order_by('timestamp').values('data', 'timestamp', 'signal', 'notification', 'device')[:50]
     errors = list(logs)
     # Aussortieren der Log Dateien ohne Fehlermeldung
     for l in logs:
         if l.get("signal") == 2:
+            # Abfrage des
             if (1000-int(l.get("data")))>50:
                 errors.remove(l)
 
