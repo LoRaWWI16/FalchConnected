385b397ae9f9809605ae33579ad31ed49ba2367c
Meikaefer <berger-meike@web.de>
2019-03-09 12:49:27 +0100

    Update Error URLS

--- falchsite/urls.py
+++ falchsite/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
-from .views import Device_TypesViewSet, DeviceViewSet, OneDeviceViewSet
+from .views import Device_TypesViewSet, DeviceViewSet, OneDeviceViewSet, ErrorViewSet
 
 urlpatterns = [
     url(r'device_types/', Device_TypesViewSet.as_view(), name="device_types-all"),
     url(r'devices/', DeviceViewSet.as_view(), name="devices-all"),
     url(r'^device/$', OneDeviceViewSet.as_view(), name="device_one")
-
+    url(r'errors/, ErrorViewSet.as_view(), name="error-all")
 ]
