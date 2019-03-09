ac56b7a4458ee3135ba07832bbe50a4522400d33
Julia Gnann <julia.gnann@exxcellent.de>
2019-03-09 10:57:24 +0100

    Rest API für logs update

--- falchsite/urls.py
+++ falchsite/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
-from .views import Device_TypesViewSet
-from .views import DeviceViewSet
+from .views import *
 
 urlpatterns = [
     url(r'device_types/', Device_TypesViewSet.as_view(), name="device_types-all"),
     url(r'devices/', DeviceViewSet.as_view(), name="devices-all"),
-    url(r'devices/', DeviceViewSet.as_view(), name="devices-one")
+    url(r'devices/', DeviceViewSet.as_view(), name="devices-one"),
+    url(r'logs/', ErrorViewSet.as_view(), name="logs-all")
 ]
