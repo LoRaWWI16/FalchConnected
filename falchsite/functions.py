25fc1716613adcbc1ff87b5bdec11fd6626b4c18
Julia Gnann <julia.gnann@exxcellent.de>
2019-03-09 12:18:41 +0100

    Rest API list all errors

--- falchsite/functions.py
+++ falchsite/functions.py
@@ -4,9 +4,12 @@ from django.shortcuts import render
 
 def error_list(request):
     logs = Log.objects.all().order_by('timestamp').values('data', 'timestamp', 'signal', 'module', 'notification')
+    errors = logs
     for l in logs:
-        print(l)
-    return logs
+        if l.signal == 2:
+            if (l.data - 1000)>50:
+                errors.remove(l)
+    return errors
 
 
 
