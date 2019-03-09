2e7cb9d69265eb40b0783df6c3e99be55fa6176c
Meikaefer <berger-meike@web.de>
2019-03-09 12:37:48 +0100

    REST API handover id in URL Test

--- falchsite/views.py
+++ falchsite/views.py
@@ -35,7 +35,7 @@ class OneDeviceViewSet(APIView):
     def get(self, request):
         if 'id' in request.GET and request.GET['id']:
             idURL = request.GET['id']
-            queryset = Device.objects.filter(id=idURL)
+            queryset = Device.objects.filter(id=1)
             serializer = DeviceSerializer(queryset, many=False)
         return Response({"devices": serializer.data})
 
