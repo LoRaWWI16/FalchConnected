2978ba19c845aac5d10173422f787c14ff04d2ae
Julia Gnann <julia.gnann@exxcellent.de>
2019-03-09 12:37:46 +0100

    Rest API list all errors fix2

--- falchsite/functions.py
+++ falchsite/functions.py
@@ -6,8 +6,8 @@ def error_list(request):
     logs = Log.objects.all().order_by('timestamp').values('data', 'timestamp', 'signal_id', 'module_id', 'notification_id')
     errors = logs
     for l in logs:
-        if l.signal_id == 2:
-            if (l.data - 1000)>50:
+        if l.get("signal_id") == 2:
+            if (1000-l.get("signal_id"))>50:
                 errors.remove(l)
     return errors
 
