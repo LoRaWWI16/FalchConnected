9b581bf2970b874b5e2cd14e8480009572403b46
Julia Gnann <julia.gnann@exxcellent.de>
2019-03-09 18:51:20 +0100

    Rest API unlimited error list

--- falchsite/functions.py
+++ falchsite/functions.py
@@ -3,13 +3,14 @@ from django.utils import timezone
 from django.shortcuts import render
 
 def error_list(request):
-    logs = Log.objects.all().order_by('timestamp').values('data', 'timestamp', 'signal', 'notification', 'device')
+    logs = Log.objects.all().order_by('timestamp').values('data', 'timestamp', 'signal', 'notification', 'device')[:1000]
     errors = list(logs)
     # Aussortieren der Log Dateien ohne Fehlermeldung
     for l in logs:
         if l.get("signal") == 2:
             # Abfrage des
-            if (1000-int(l.get("data")))>50:
+            if (int(Device_Type.objects.filter(id=int(l.get("device"))).values("service_hours").first()["service_hours"])
+                - int(l.get("data")))>50:
                 errors.remove(l)
 
     # Aufschluesselung der Device ID zu Device Name und Hinzufuegen der Notification
