698e5642fc1f0b4370bc828bbe2f806ba41b30c6
Julia Gnann <julia.gnann@exxcellent.de>
2019-03-09 14:07:45 +0100

    Rest API list all errors fix7

--- falchsite/functions.py
+++ falchsite/functions.py
@@ -3,10 +3,10 @@ from django.utils import timezone
 from django.shortcuts import render
 
 def error_list(request):
-    logs = Log.objects.all().order_by('timestamp').values('data', 'timestamp', 'signal_id', 'module_id', 'notification_id')
+    logs = Log.objects.all().order_by('timestamp').values('data', 'timestamp', 'signal', 'module_id', 'notification_id')
     errors = list(logs)
     for l in logs:
-        if l.get("signal_id") == 2:
+        if l.get("signal") == 2:
             if (1000-int(l.get("data")))>50:
                 errors.remove(l)
     return errors
